db7ee96851064e031988b45b10655083e3e6cbe0
Isaiah Mann <contact@isaiahmann.com>
2017-03-07 14:07:38 -0500

    Simple printing program

---
+++ CTutorial/name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+     int a = 3;
+     int b = 5;
+     int c = 0;
+     char firstName[] = "Isaiah";
+     char lastName[] = "Mann";
+     char fullName[100];
+     c = a + b;
+     printf("%s %s %d\n", firstName, lastName, c);
+     return 0;
+}
