b39b611f6aa7e77b67af68ba09356e019a6ddfeb
Isaiah Mann <contact@isaiahmann.com>
2017-03-07 13:50:40 -0500

    Adding string operations

--- CTutorial/helloworld.c
+++ CTutorial/helloworld.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-     printf("\nHello World!\n");
+     char hello[] = "Hello";
+     char world[] = "World!";
+     char sentence[100] = "";
+
+     strcpy(sentence, hello); // sentence <- "Hello"
+     strcat(sentence, " "); // sentence <- "Hello "
+     strcat(sentence, world); // sentence <- "Hello World!"
+
+     printf("sentence = %s\n", sentence);
+
      // Means "no error":
      return 0;
 }
