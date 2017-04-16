3a865f64b41d152bdb4948fd9b7585bd36fa5107
Isaiah Mann <contact@isaiahmann.com>
2017-04-16 13:41:16 -0400

    Pushing game of life MPI code

---
+++ GameOfLifeMPI/GameOfLifeMPIN.c
@@ -0,0 +1,13 @@
+/**
+ * Author: Isaiah Mann
+ * Description: Runs Game of Life on MPI for N nodes
+ * Adapted From: 2 Process Version by D. Thiebaut (CSC352, S17)
+ */
+
+ #include <stdio.h>
+ #include <strings.h>
+ #include <stdlib.h>
+ #include <time.h>
+ #include <unistd.h>
+ #include "mpi.h"
+ 
